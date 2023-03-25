2edde12d22882581e3ea821e7a5d19d7d4b9e032
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-26 02:42:32 +0630

    deployed fix

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,4 @@
 export const env = {
-    APP_URL: "http://localhost:3000",
+    APP_URL: "https://sharememo-qx4zj5lnz-mr-ben-jeckson.vercel.app",
     API_URL: "https://apis-sharememo.onrender.com"
 };
