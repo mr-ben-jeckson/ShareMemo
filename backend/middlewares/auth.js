7b6b74c5f37e49b2c8e254704b7424edef34172a
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-26 01:25:50 +0630

    fix for render

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ export const verfiyToken = async(req, res, next) => {
         if(token.startsWith("Bearer ")) {
             token = token.slice(7, token.length).trimLeft();
         }
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(token, `${process.env.JWT_SECRET}`);
         req.user = verified;
         next();
     } catch (err) {
