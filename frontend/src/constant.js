f4bdc3edf79d9bd039de1575f56dcc87d506ca42
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-26 02:44:43 +0630

    fix vercel domain

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,4 @@
 export const env = {
-    APP_URL: "https://sharememo-qx4zj5lnz-mr-ben-jeckson.vercel.app",
+    APP_URL: "https://sharememo-app.vercel.app/",
     API_URL: "https://apis-sharememo.onrender.com"
 };
